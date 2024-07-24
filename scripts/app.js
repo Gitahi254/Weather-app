931ef5d00d25610439a66a3f3443a9c09a6492d9
Gitahi <116303227+Gitahi254@users.noreply.github.com>
2024-07-24 06:55:33 -0800

    Update app.js
    I have updated the app.js to use object-oriented programming in JavaScript.

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,7 @@ const details = document.querySelector('.details');
 
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = (data) => {
 
@@ -48,17 +49,6 @@ const updateUI = (data) => {
 };
 
 
-
-
-
-const updateCity = async (city) =>{
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
-    return{cityDets,weather};
-};
-
-
-
 // submit event
 cityForm.addEventListener('submit', e =>{
     // prevent default action
@@ -68,16 +58,17 @@ cityForm.addEventListener('submit', e =>{
     cityForm.reset();
 
     // update the UI with new city
-    updateCity(city)
+    forecast.updateCity(city)
         .then(data =>updateUI(data))
         .catch(err => console.log(err));
-     //SET LOCAL STORAGE
+
+        //SET LOCAL STORAGE
         localStorage.setItem('city', city);
 });
 
 
 if(localStorage.getItem('city')){
-    updateCity(localStorage.getItem('city'))
+    forecast.updateCity(localStorage.getItem('city'))
     .then(data => updateUI(data))
     .catch(err => console.log(err))
 };
